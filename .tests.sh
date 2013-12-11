c8d6a16d1de0ba87fe249fc19b7daad64e3b833f
caiguanhao <caiguanhao@gmail.com>
2013-12-11 22:33:39 +0800

    add text color and fix typo;

--- .tests.sh
+++ .tests.sh
@@ -2,13 +2,13 @@
 
 TEST()
 {
-  echo ">" $BASH "$@"
+  echo -e "\x1B[37m>" $BASH "$@""\x1B[39m"
   $BASH "$@"
   if [ $? -eq 0 ]; then
-    echo "Test of $1 was passed."
+    echo -e "\x1B[92mTest of $1 was passed.\x1B[39m"
     echo
   else
-    echo "Test of $1 was failed."
+    echo -e "\x1B[91mTest of $1 was failed.\x1B[39m"
     exit 1
   fi
 }
@@ -21,5 +21,5 @@ TEST coord2point.sh 113.331110, 23.112097
 TEST point2addr.sh 12616098.76, 2628657.08
 TEST gmaps2bdmaps.sh 23.10641, 113.32449
 
-echo "All test were passed."
+echo -e "\x1B[92mAll tests were passed.\x1B[39m"
 exit 0
